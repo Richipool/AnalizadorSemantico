688190f9ff474e2809d26e43b1ccc07448aa13c7
SamantaAlfaro <samantasoul2612@gmail.com>
2019-11-13 17:55:13 -0600

    avance 3

--- Solucion.py
+++ Solucion.py
@@ -1,12 +1,62 @@
 #vamos a morir
 
+import nltk
+from nltk.tokenize import word_tokenize
+import vector
+
 class Var:
-    def __init__(self, _type, _name, _line, _val = none):
+    def __init__(self, _type, _name, _line, _val = None):
         self.name = _name
         self.type = _type
         self.line = _line
         self.val = _val
 
+errors = []#vector de errores
+def saveErrors(error, Var var):
+    if(error is 1):#variable no declarada
+        errors.append("variable" + var._name+" no declarada")
+    elif(error is 2):#valor de retorno no coincide con la declaracion de funcion
+        errors.append("valor de retorno no coincide con la declaracion de funcion")
+    elif(error is 3):#asignacion de tipo incorrecto
+        errors.append("asignacion de tipo incorrecto")
 
-def main():
-    #hacer las varas aqui!
+
+typesArray = ["","","",""]
+
+"""ARCHIVO"""
+tipo, nombre, linea, valor#valr
+globalTable = {} 
+lineCounter = 1#para ir contanto las lineas
+f = open("pruebas.txt", encoding = "utf8")
+#cadena1 = f.read()....sin el encoding no funca
+
+#print(f.read())
+#linea = f.readline()
+
+for tokens in f:
+    i = word_tokenize(tokens)
+    for x in i:
+        if x[i] is "int" or x[i] is "void" or x[i] is "float" or x[i] is "string":
+            tipo = x[i]
+        else 
+
+    #print(i)
+
+
+
+f.close()
+
+
+"""def main():
+    #hacer las varas aqui!...solo para que se vea bonito y separar las cosas...pero se puede quitar
+    
+  
+
+    #string textLine#ir extrayendo linea por linea
+ 
+
+
+
+#se llama al main
+main()
+"""
