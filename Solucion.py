47bf3f4390bec682ae31b36d82c6ad997a393e93
SamantaAlfaro <samantasoul2612@gmail.com>
2019-11-12 22:04:07 -0600

    continuacion

--- Solucion.py
+++ Solucion.py
@@ -1,1 +1,12 @@
 #vamos a morir
+
+class Var:
+    def __init__(self, _type, _name, _line, _val = none):
+        self.name = _name
+        self.type = _type
+        self.line = _line
+        self.val = _val
+
+
+def main():
+    #hacer las varas aqui!
