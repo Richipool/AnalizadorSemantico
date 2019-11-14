06111d5930b4295947d37b8332613e5d078259da
SamantaAlfaro <samantasoul2612@gmail.com>
2019-11-13 23:00:07 -0600

    quinto comimt

--- Solucion.py
+++ Solucion.py
@@ -2,6 +2,7 @@
 
 import nltk
 from nltk.tokenize import word_tokenize
+import tokenize
 
 class Var:
     def __init__(self, _type, _name, _line, _val = None):
@@ -15,11 +16,11 @@ class Var:
 
 errors = []#vector de errores
 def saveErrors(error, var):
-    if(error is 1):#variable no declarada
+    if(error == 1):#variable no declarada
         errors.append("variable" + var._name+" no declarada")
-    elif(error is 2):#valor de retorno no coincide con la declaracion de funcion
+    elif(error == 2):#valor de retorno no coincide con la declaracion de funcion
         errors.append("valor de retorno no coincide con la declaracion de funcion")
-    elif(error is 3):#asignacion de tipo incorrecto
+    elif(error == 3):#asignacion de tipo incorrecto
         errors.append("asignacion de tipo incorrecto")
 
 
@@ -41,41 +42,46 @@ def printHasTable():
 #"""ARCHIVO"""
 tipo= None
 nombre= None
-linea= None 
-valor = None#valr
- 
+linea = None 
+valor = None #valr 
 lineCounter = 1#para ir contanto las lineas
-f = open("pruebas.txt", encoding = "utf8")
-#cadena1 = f.read()....sin el encoding no funca
-
+f = open("pruebas.txt", "r")#encoding = "utf8"...ahora funciona sin esto
 #print(f.read())
-#linea = f.readline()
+#for x in f:
+#    linea = word_tokenize(x)
+    #print(linea)
+#print(linea)
+#print(len(linea))
+#print(type(linea))
+#print("\n")
 
-for tokens in f:
+for tokens in f:#recorre las lineas de codigo del archivo fuente
     i = word_tokenize(tokens)
-    for x in i:
-        if  i[3] is '=':
-            if x[i] is "int" or x[i] is "void" or x[i] is "float" or x[i] is "string":
-                tipo = x[i]
-            #elif x[i] is 
+    print(i)
+#    print(type(i))
+#    print(len(i))
+    
+    #print("espacio")
 
-    #print(i)
+#    for x in i:
+    tipo = i[0]
+    nombre = i[1]
 
-
-
+    print("tipo:" + tipo)
+    print("nombre:" + nombre)
+#        if  x[2] == '=':
+#            print(i)
+#if x== "int" or x == "void" or x == "float" or x == "string":
+#tipo = x
+#print(tipo)
+#elif x[i] is 
 f.close()
 
 
 """def main():
     #hacer las varas aqui!...solo para que se vea bonito y separar las cosas...pero se puede quitar
-    
-  
-
     #string textLine#ir extrayendo linea por linea
  
-
-
-
 #se llama al main
 main()
 """
