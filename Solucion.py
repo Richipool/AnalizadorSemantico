2b5ba089f73b72c845c1b5fc168e9c45a410cc0a
Cristopher Arce Diaz <christopher.ardi1999@gmail.com>
2019-11-15 15:15:50 -0600

    primera version final

--- Solucion.py
+++ Solucion.py
@@ -22,8 +22,14 @@ def saveToTable(var):
 
 def printHasTable():
     for key in globalTable:
-        print(key, globalTable[key].type, globalTable[key].val, globalTable[key].line)
+        print(key, globalTable[key].type, globalTable[key].val, globalTable[key].line, globalTable[key].father)
 
+def esString(w):#recibe 
+	#cout << w;
+	return(w[0]== -30 and w[w.size()-1] == -99 or w[0] == 34 and w[w.size() - 1] == 34 or w[0] == 39 and w[w.size() - 1] == 39)
+
+def isNumber(s):
+    return (s.replace('.','',1).isdigit())
 
 """AQUI EMPIEEZA EL COPY PASTE/TRADUCCION SALVAJE"""
 
@@ -37,7 +43,6 @@ especiales = ["+","-" , ";" ,"*",",", "/","=","==","!=","<",">" ,")","{","}","("
 #"""ARCHIVO"""
 word =None
 word1 = None
-word2 = None
 lineCounter = 1#para ir contanto las lineas
 
 
@@ -46,107 +51,57 @@ stPadre = queue.LifoQueue()#para las funciones
 
 f = open("pruebas.txt")#encoding = "utf8"...ahora funciona sin esto
 line = f.readlines()#una lista de lineas
+f.close()
+
 for x in line:#ir linea por linea del archivo
     word = x.split()#hace tokens por espacios...una lista de cada palabra 
     #print()#linea por linea
     for y in word:#ir recorriendo los tokens de word
         #print(y)
-        if word == "}":
+
+        if y == "}":
             stPadre.get()
-        if word == "if" or word == "while":
-            var = Var(word, "undefined")
+        if y == "if" or y == "while":
+            var = Var(y, "undefined")
             var.id = "statement"
-            if len(stPadre)>0:#pila de funciones
-                var.father = stPadre.queue[stPadre.qsize -1]#dudas por aqui de si el "top" es asi
+            if stPadre.qsize():#pila de funciones
+                var.father = stPadre.queue[stPadre.qsize() -1]#dudas por aqui de si el "top" es asi
             st.put(var)#mete al stack la variale
-            stPadre.put(word)#el identificador de la funcion
-        if word in variables or word == "(":
-            if word == "(":
+            stPadre.put(y)#el identificador de la funcion
+        if y in variables or y == "(":
+            if y == "(":
                 if not st.empty():
-                    if st.queue[st.qsize-1].id != "statement":
-                        st.queue[st.qsize-1].id = "funcion"
-                        stPadre.put(st.queue[st.qsize-1].type)
+                    if st.queue[st.qsize()-1].id != "statement":
+                        st.queue[st.qsize()-1].id = "funcion"
+                        stPadre.put(st.queue[st.qsize()-1].type)
                         #eliminar la funcion del diccionario, mediante la llave "name"
-                        del globalTable[st.queue[st.qsize-1].name]
+                        del globalTable[st.queue[st.qsize()-1].name]
             else: #se supone que la siguiente palabra el el nombre de la variable
                 word1 = word[1]#no se si seria asi.......DUDA
-                print(word1)
-                var = Var(word, word1)
-                if st.qsize>0:
-                    var.father = stPadre.queue[stPadre.qsize-1]
-                    if word == "}":
+                #print(word1)
+                var = Var(y, word1)
+                if stPadre.qsize():
+                   # print(stPadre.qsize())
+                    var.father = stPadre.queue[stPadre.qsize()-1]
+                    if y == "}":
                         stPadre.get()
                 var.id = "variable"
-                sr.put(var)
+                st.put(var)
                 saveToTable(var)#el guarda en el diccionario
-        elif(not word in especiales and not word.isdigit() and not word in statements and ):
-            #
-                
-                    
-
-f.close()
-
-
-#print(f.read())
-#for x in f:
-#    linea = word_tokenize(x)
-    #print(linea)
-#print(linea)
-#print(len(linea))
-#print(type(linea))
-#print("\n")
-flag = True
+        elif((not y in especiales) and (not isNumber(y)) and (not y in statements) and (not esString(y))):
+       # and (not isinstance(y, str))):
+            aux = globalTable.get(y)
+            if aux is None:
+                print("Error -linea " , lineCounter ,": " + y + " no esta declarada\n")
+    lineCounter+=1
+print("\n\nStack de variables\n\n")
+while not st.empty():
+    print(st.queue[st.qsize()-1].name + "\n")
+    st.get()
+print("\n\nHash Table\n\n")
+printHasTable()
 
 """
-for tokens in file:#recorre las lineas de codigo del archivo fuente
-    #i = word_tokenize(tokens)
-    i = file.readlines()
-    print(i)
-#    for x in i:#ir recorriendo "los tokens" de 
-#        print(i)
-    if(i[2] == "="):#si asigna valores
-        if(i[0] != 'void' and i[0] != 'int' and (i[0])!= 'float' and i[0] != 'string' or (i[0] == '=' or i[0]==';')):
-            tipo = None
-        else: tipo = i[0]
-        if(i[1] != 'void' and i[1] != 'int' and (i[1])!= 'float' and i[1] != 'string' or (i[1] == '=' or i[1]==';')):
-            nombre = i[1]
-        else: nombre = None 
-        
-        valor = i[3]
-        aux = 3#donde esta la posicion atual en el vector
-        while(flag is True and aux != len(i)):
-           # if ('"' in i[aux]):
-                #flag = False
-            if(i[0] == 'string'):
-                if('"' in i[aux+1]):
-                    valor = valor + ' ' + i[aux+1]
-                    flag = False
-                if(flag is True):
-                    valor = valor + ' ' + i[aux+1]
-            aux +=1
-        #obj = Var(tipo, nombre, valor,None, lineCounter)
-        #saveToTable(obj)
-    elif(i[2]== ";"):#por si es solo una declaracion
-        if(i[0] != "void" or i[0] != "int" or (i[0])!= "float" or i[0] != "string" and i[0] == "=" or i[0]==";"):
-            tipo = None
-        else: tipo = i[0]
-        if(i[1] != "void" or i[1] != "int" or (i[1])!= "float" or i[1] != "string" and i[1]!= "=" or i[1]!=";"):
-            nombre = None
-        else: nombre = i[1]
-        #obj = Var(tipo, nombre, valor,None, lineCounter)
-        #saveToTable(obj)
-    #lineCounter +=1
-
-##    if(i[3] == '"'):
-#        valor = i[4]
-#    valor = i[3][:len(i[3])-1]
-
-
-
-#    print("tipo:" + tipo)
-#    print("nombre:" + nombre)
-
-
 print("ESPACIO PARA VER VARAS")
 #printHasTable()
 """
